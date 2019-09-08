b9680006f94db106ba1c099cf464405969bdea0a
Bressani Stéphane <s.bressani@bluewin.ch>
2019-09-08 18:07:56 +0200

    ajout d'un timer pour tester les frequences

--- SoundSynth/main.cpp
+++ SoundSynth/main.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 #include <string>
+
+#include <chrono>
+#include <thread>
 using namespace std;
 
 #include "olcNoiseMaker.h"
@@ -30,6 +33,9 @@ double MakeNoise(double dTime)
 		return 0.2;
 	else
 		return -0.2;
+	// Chord sin
+	//double dOutput = 1.0 * (sin(dFrequencyOutput * 2 * 3.14159 * dTime) + sin((dFrequencyOutput + 20) * 2.0 * 3.14159 * dTime));
+	//return dOutput * 0.4;
 }
 
 int main()
@@ -48,17 +54,51 @@ int main()
 	// Link noise function with sound machine
 	sound.SetUserFunction(MakeNoise);
 
+	//double dOctaveBaseFrequency = 440; // C 110.0; // A2
+	double dOctaveBaseFrequency = 110.0; // A2
+	double d12ThRootOF2 = pow(2.0, 1.0 / 12.0); // puissance
+
 	while (1) 
 	{
+
+
+
+		/*
+		// Add a keyboard like a piano
+		bool bKeyPRessed = false;
+		for (int k = 0; k < 15; k++)
+		{
+			if (GetAsyncKeyState((unsigned char)("YSXCFVGBNJMK\xbcL\xbe"[k])) & 0x8000)
+			{
+				dFrequencyOutput = dOctaveBaseFrequency * pow(d12ThRootOF2, k);
+				bKeyPRessed = true;
+			}
+		}
+		if (!bKeyPRessed)
+			dFrequencyOutput = 0.0;
+			*/
+
+		// 12
+
+		for (int i = 1; i <= 12; i++)
+		{
+			std::this_thread::sleep_for(std::chrono::milliseconds(500));
+			dFrequencyOutput = dOctaveBaseFrequency * pow(d12ThRootOF2, i);
+		}
+
+		/*
 		// Add a keyboard
 		if (GetAsyncKeyState('A') & 0x8000)
 		{
-			dFrequencyOutput = 440.0;
+			//dFrequencyOutput = 440.0;
+			//for (int i = 1; i <= 12; i++)
+			dFrequencyOutput = dOctaveBaseFrequency * pow(d12ThRootOF2, 0);
 		}
 		else
 		{
 			dFrequencyOutput = 0.0;
 		}
+		*/
 	}
 
 	return 0;
